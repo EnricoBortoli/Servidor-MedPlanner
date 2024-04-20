d1066ad0ca7ed9d1097a84d61566afe780bc04e4
EnricoBortoli <enricotb03@gmail.com>
2024-04-20 17:20:14 -0300

    primeiro commit

---
+++ src/main/java/medplanner/MedplannerApplication.java
@@ -0,0 +1,13 @@
+package medplanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedplannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedplannerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/medplanner/MedplannerApplicationTests.java
@@ -0,0 +1,13 @@
+package medplanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedplannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
