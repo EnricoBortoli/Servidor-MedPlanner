3f3814b6ef90cdbc3c55f30078ea834cb5fc70e3
EnricoBortoli <enricotb03@gmail.com>
2024-05-05 20:53:49 -0300

    Alterada a versão do projeto, devido a erro de Cors

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+
+--Inclusão de um usuario administrador padrão
+insert into usuario (nome, username, cpf, password, situacao) value ('Administrador do sistema', 'ADMIN', '11122233344', 'abc.123', 'A');
