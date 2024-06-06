ddd2f7cf5acead8e9c739cb97d2ee4b9014bc4ad
Louisii <louisimtd@outlook.com>
2024-06-06 15:16:55 -0300

    revert changes usuario repo

--- src/main/java/medplanner/repository/UsuarioRepository.java
+++ src/main/java/medplanner/repository/UsuarioRepository.java
@@ -1,24 +1,23 @@
 package medplanner.repository;
 
+
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.security.core.userdetails.UserDetails;
 
-import medplanner.model.Profissional;
 import medplanner.model.Usuario;
-
-public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
-
+public interface UsuarioRepository extends JpaRepository<Usuario, Long>  {
+    
     @Query(value = "SELECT * FROM Usuario p WHERE CAST(p.id_Usuario AS char) LIKE ?1%", nativeQuery = true)
     List<Usuario> findAllByIdUsuario(String id);
 
     @Query(value = "SELECT * FROM Usuario p WHERE p.nome LIKE ?1%", nativeQuery = true)
     List<Usuario> findAllByNomeUsuario(String nome);
 
-    @Query("SELECT p FROM Profissional p WHERE p.cpf = ?1")
-    Profissional findByCPF(String cpf);
+    @Query(value = "SELECT * FROM usuario p WHERE p.cpf = ?1", nativeQuery = true)
+    Usuario findByCPF(String cpf);
 
     UserDetails findByUsername(String username);
 }
